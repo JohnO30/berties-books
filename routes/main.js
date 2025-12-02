4e9de6fcfc8abf4aab6d448e86037c8425cb7c96
John-O <johnoladosu@outlook.com>
2025-12-02 16:56:21 +0000

    Lab 9a

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,7 @@
 // Create a new router
 const express = require("express");
 const router = express.Router();
+const request = require('request');
 
 // Handle home route
 router.get('/', function (req, res, next) {
@@ -10,6 +11,26 @@ router.get('/', function (req, res, next) {
 // About page route
 router.get('/about', function (req, res, next) {
   res.render('about.ejs');
+});
+
+// Weather page route
+router.get('/weather', function (req, res, next) {
+  let apiKey = '13400a408d993046228e527c0dd4850e';
+  let city = req.query.city || 'london';
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+                     
+  request(url, function (err, response, body) {
+    if(err){
+      next(err);
+    } else {
+      var weather = JSON.parse(body);
+      if (weather !== undefined && weather.main !== undefined) {
+        res.render('weather.ejs', { weather: weather, city: city });
+      } else {
+        res.render('weather.ejs', { weather: null, city: city, error: 'No data found' });
+      }
+    } 
+  });
 });
 
 // Export the router object so index.js can access it
