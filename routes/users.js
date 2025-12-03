23c5a1682f659a2108b615943b3bd34c55e11ff7
John-O <johnoladosu@outlook.com>
2025-12-03 10:11:35 +0000

    Fix login redirection path in redirectLogin function

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,7 @@ const saltRounds = 10;
 
 const redirectLogin = (req, res, next) => {
   if (!req.session.userId) {
-    res.redirect('/users/login');
+    res.redirect('/login');
   } else {
     next();
   }
@@ -103,7 +103,6 @@ router.get("/list", redirectLogin, function (req, res, next) {
     res.render("listusers.ejs", { users: rows });
   });
 });
-
 
 // 
 router.post("/loggedin", function (req, res, next) {
